4087d68707b228c0631cbfcdc632d7f20853da62
Exbyte112 <ismailaabubakar112@gmail.com>
2024-06-13 20:29:19 +0100

    update uno

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from flask import Flask, render_template, request, jsonify
-import json
+from flask import Flask, render_template, request
 import pymongo
 from pymongo import MongoClient
 
+# MongoDB connection
 cluster = MongoClient(
     "mongodb+srv://exbyte:1234@cluster0.aw1f22q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 )
-
 db = cluster["KronosTwikit"]
 collection = db["KronosTwikit"]
 
@@ -19,9 +18,9 @@ def edit_db():
 @app.route("/save", methods=["POST"])
 def save_changes():
     # get form data
-    main_bot_email = request.form.get("main_bot_email")
-    main_bot_password = request.form.get("main_bot_password")
-    main_bot_username = request.form.get("main_bot_username")
+    main_bot_email = request.form.get("email")
+    main_bot_password = request.form.get("password")
+    main_bot_username = request.form.get("username")
     
     # update data dictionary
     data = {
@@ -31,10 +30,16 @@ def save_changes():
     }
     
     # save data to database
-    collection.insert_one(data)
+    collection.update_one(
+        {"_id": 0},
+        {"$set": data},
+        upsert=True
+    )
     
     # return success message as JSON
     return render_template("save.html", data=data)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True, port=0000)
+    app.run(debug=True, port=int(os.environ.get("PORT", 5000)))
